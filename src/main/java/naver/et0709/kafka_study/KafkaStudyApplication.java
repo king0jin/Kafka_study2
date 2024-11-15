0bf642e4e4b23d4391d90733031887fd6bab2bd5
king0jin <et0709@naver.com>
2024-11-15 21:33:00 +0900

    create project

---
+++ src/main/java/naver/et0709/kafka_study/KafkaStudyApplication.java
@@ -0,0 +1,13 @@
+package naver.et0709.kafka_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/naver/et0709/kafka_study/KafkaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package naver.et0709.kafka_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
